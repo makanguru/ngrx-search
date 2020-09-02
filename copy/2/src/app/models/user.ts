e1f45cf9f54ad6ca5fc9e1636afccc9bc06790e3
unknown <makprg@ukr.net>
2020-09-02 10:32:53 +0300

    delete

--- copy/2/src/app/models/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-  id: number;
-  username: string;
-  name: string;
-  language: string;
-  owner: {};
-}
--- copy/2/src/app/shared/ui.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-export const START_LOADING = createAction('[UI] Start Loading');
-export const STOP_LOADING = createAction('[UI] Stop Loading');
